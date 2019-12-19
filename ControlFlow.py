b78117249dd75b5ee9f2f89f02beba84769018ae
Patricio Ortiz <02patricio.ortiz@gulllakecs.org>
2019-12-19 05:20:05 -0800

    Add 1 Through 10 While Loop

--- ControlFlow.py
+++ ControlFlow.py
@@ -3,7 +3,7 @@ Programmer: PattyDaddy
 Date: 12.16.19
 Program: Guess My Number
 """
-
+"""
 myNumber = 7
 
 print("\nGuess a number between One & Ten\n")
@@ -18,3 +18,20 @@ while guess != myNumber:
     guess = int(input("Enter a Guess: "))
 
 print ("\nCongrats homie, you's is right!!")
+"""
+
+"""
+Programmer: PattyDaddy
+Date: 12.19.19
+Program: 1 Through 10
+"""
+
+x = 1
+
+# While loop will see if a condition has been met
+# If not it will run again until the condition
+# has been met
+
+while x <= 10:
+    print(x)
+    x = x + 1
