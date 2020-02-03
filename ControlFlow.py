077629623a12a287fb1850bf9ab755334775d27d
Patricio Ortiz <02patricio.ortiz@gulllakecs.org>
2020-02-03 05:35:00 -0800

    Add Greeting

---
+++ ControlFlow.py
@@ -0,0 +1,15 @@
+# Date: 2.3.2020
+# Greeting Function
+
+name = input("What is your name: ")
+
+
+# Create Functions Here
+def greeting():
+    print("Hello there " + name + ".")
+
+
+
+# Call Functions Here
+
+greeting()
