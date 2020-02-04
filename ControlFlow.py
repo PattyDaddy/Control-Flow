d79b41c75f93fc1261f24cebaac24cc127f90758
Patricio Ortiz <02patricio.ortiz@gulllakecs.org>
2020-02-04 05:03:35 -0800

    Add Function & Parameter

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,22 +1,32 @@
-# Date: 2.3.2020
-# Greeting Function
 
-# Declare Global Variables here...
+## Date: 2.3.2020
+## Programmer: Patricio Ortiz
 
-name = input("What is your name: ")
+## Declare Global Variables here...
+
+# name = input("What is your name: ")
 x = 15
 
-# Create Functions Here...
+## Create Functions Here...
+
+## Greeting Function
 def greeting():
     print("Hello there " + name + ".")
     print(x)
 
+## Functions & Local Variable x
 def printSomething():
     x = 3
     print(x)
 
-# Call Functions Here...
+# Functions & Parameters
+def printNumber(age): ##function name = printNumber with a PARAMETER of age
+    print(age)
 
-greeting()
-printSomething()
-print(x)
+## Call Functions Here...
+
+# greeting()
+# printSomething()
+# print(x)
+printNumber(15)
+printNumber(16)
