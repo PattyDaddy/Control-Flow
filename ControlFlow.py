6a13a85f32ee41ef3367bc4b393aeb2f5beeb846
Patricio Ortiz <02patricio.ortiz@gulllakecs.org>
2020-02-05 05:17:09 -0800

    Add separation between Functions with Titles

--- ControlFlow.py
+++ ControlFlow.py
@@ -4,7 +4,7 @@
 
 ## Declare Global Variables here...
 
-# name = input("What is your name: ")
+name = input("What is your name: ")
 x = 15
 
 ## Create Functions Here...
@@ -40,12 +40,27 @@ def printMultipleTimes(string, times): ## ILoveComputerScience has STRING a data
 
 ## Call Functions Here...
 
-# greeting()
-# printSomething()
-# print(x)
-# printNumber(15)
-# printNumber(16)
-# printTwoNumbers(23,78)
-# printTwoNumbers(45)
-# printSum(1,17)
+print("\n***Greetings Function***\n")
+greeting()
+print("\n***Print Something Function***\n")
+printSomething()
+
+print(x)
+
+print("\n***Print Number Function***\n")
+printNumber(15)
+printNumber(16)
+
+print("\n***Print Two Numbers Function***\n")
+printTwoNumbers(23,78)
+
+print("\n***Default Parameter Values Function***\n")
+printTwoNumbers(45)
+
+print("\n***Print Sum Function***\n")
+printSum(1,17)
+
+print("\n***Print Multiple Times Function***\n")
 printMultipleTimes("ILoveComputerScience", 13)
+
+print("\n***I hope you enjoyed my functions!!***\n")
